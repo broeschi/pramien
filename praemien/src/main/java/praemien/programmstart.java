921f1c6fcf5fe8eefc7f94614ad2540518d845f9
broeschi <39580893+broeschi@users.noreply.github.com>
2024-11-04 17:03:15 +0100

    erste Version

---
+++ praemien/src/main/java/praemien/programmstart.java
@@ -0,0 +1,13 @@
+package praemien;
+
+/**
+ * Hello world!
+ *
+ */
+public class programmstart 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
