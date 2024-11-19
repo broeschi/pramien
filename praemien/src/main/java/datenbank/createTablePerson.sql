01039c336e634573d03388cb815fcf6c4941dbe8
15183 <15183@DESKTOP-RT6STD1>
2024-11-19 14:11:54 +0100

    createTable ergänzt

--- praemien/src/main/java/datenbank/createTablePerson.sql
+++ praemien/src/main/java/datenbank/createTablePerson.sql
@@ -40,3 +40,61 @@ CREATE TABLE Meisterschaften (
     meisterName VARCHAR(255) NOT NULL,
     meisterAbk VARCHAR(255) NOT NULL
 );
+
+
+CREATE TABLE RennArt (
+    rennArtId INT PRIMARY KEY AUTO_INCREMENT,
+    rennArtName VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE KmEntschaedigung (
+    kmEndschId INT PRIMARY KEY AUTO_INCREMENT,
+    kmEntschEinzelRennenId INT NOT NULL,
+    kmEntschPersonId INT NOT NULL,
+    kmEntschAnzahlFahrer INT NOT NULL,
+    kmEntschKmGeld DOUBLE NOT NULL,
+    kmEntschKm INT NOT NULL
+);
+
+CREATE TABLE Disziplin (
+    disziplinId INT PRIMARY KEY AUTO_INCREMENT,
+    disziplinName VARCHAR(255) NOT NULL,
+    disziplinAbk VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE EinzelRennen (
+    einzelRennenId INT PRIMARY KEY AUTO_INCREMENT,
+    einzelRennenRennDatum DATE NOT NULL,
+    einzelRennenName VARCHAR(255) NOT NULL,
+    einzelRennenDisziplinId INT NOT NULL,
+    einzelRennenSaisonId INT NOT NULL,
+    einzelRennenPersonId INT NOT NULL,
+    einzelRennenRennArtId INT NOT NULL,
+    einzelRennenRang INT NOT NULL,
+    einzelRennenLink VARCHAR(255),
+    einzelRennenPunkte INT NOT NULL
+);
+
+CREATE TABLE Praemien (
+    praemieId INT PRIMARY KEY AUTO_INCREMENT,
+    praemienSaisonId INT NOT NULL,
+    praemienEinzelRennenId INT NOT NULL,
+    praemienHelfereinsatz BOOLEAN NOT NULL,
+    praemienEm BOOLEAN NOT NULL,
+    praemienWm BOOLEAN NOT NULL,
+    praemienOs BOOLEAN NOT NULL,
+    praemienTsp BOOLEAN NOT NULL,
+    praemienEmBetrag INT NOT NULL,
+    praemienWmBetrag INT NOT NULL,
+    praemienOsBetrag INT NOT NULL,
+    praemienTspBetrag INT NOT NULL,
+    praemienLizenzbeitrag INT NOT NULL,
+    praemienTalentsichtung INT NOT NULL
+);
+
+CREATE TABLE Saison (
+    saisonId INT PRIMARY KEY AUTO_INCREMENT,
+    saisnonJahr INT NOT NULL,
+    saisonVon DATE NOT NULL,
+    saisonBis DATE NOT NULL
+);
